065410f6fda51aeca7901876a2d49fa76aa45353
Brook Yang <uedsky@gmail.com>
2016-07-06 06:49:18 +0800

    init

---
+++ index.js
@@ -0,0 +1,18 @@
+
+/*!
+ * do some thing
+ */
+
+/**
+ * sum
+ * @param  {[type]} a first 
+ * @param  {[type]} b second
+ * @return {[type]}   sum a + b
+ */
+function sum(a, b) {
+	return a + b;
+}
+
+module.exports = sum;
+
+
