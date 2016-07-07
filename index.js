7eb77cf213f288d49d23e3d31976ab28e9ff7ac5
uedsky <yangguangsmail@gmail.com>
2016-07-07 10:23:17 +0800

    Update index.js

--- index.js
+++ index.js
@@ -8,6 +8,9 @@
  * @param  {[type]} a first 
  * @param  {[type]} b second
  * @return {[type]}   sum a + b
+ * @playground
+ * var a = require('./index');
+ * a(1, 3)
  */
 function sum(a, b) {
 	return a + b;
